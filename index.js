6bb8ee95f7d6ff0c766cbde1d546daf5ab1ab7ad
cassac <hicassac@gmail.com>
2016-10-30 17:51:20 -0700

    creates game board and sets state

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-const App = () => <div>Hello World</div>;
+class Game extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      board: []
+    }
+  }
 
-render(<App/>, document.getElementById('app'));
+  componentWillMount() {
+    this.createBoard();
+  }
+
+  createBoard() {
+    let board = [];
+    for (var i = 0; i < 6; i++) {
+      let row = []
+      for (var j = 0; j < 7; j++) {
+        row.push(null);
+      }
+      board.push(row);
+      row = [];
+    }
+    this.setState({board}, () => {
+      console.log(this.state.board);
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+
+}
+
+render(<Game/>, document.getElementById('app'));
