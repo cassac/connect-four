621d9c4365bad3d6d3b7b9685839c78a54a776c3
cassac <hicassac@gmail.com>
2016-10-30 18:12:27 -0700

    implements materialui react

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 class Game extends Component {
   constructor(props) {
@@ -7,6 +11,10 @@ class Game extends Component {
     this.state = {
       board: []
     }
+  }
+
+  getChildContext() {
+    return { muiTheme: getMuiTheme(baseTheme) };
   }
 
   componentWillMount() {
@@ -28,14 +36,32 @@ class Game extends Component {
     })
   }
 
+  displayBoard() {
+    const style = {
+      height: 100,
+      width: 100,
+      margin: 20,
+      textAlign: 'center',
+      display: 'inline-block',
+    };
+
+    // need to change mapping. must map paper pieces to each row
+    return this.state.board.map((piece, idx) => <Paper key={idx} style={style} zDepth={1} circle={true} />);
+
+  }
+
   render () {
     return (
       <div>
-        Hello World
+        {this.displayBoard()}
       </div>
     )
   }
 
 }
+
+Game.childContextTypes = {
+    muiTheme: React.PropTypes.object.isRequired,
+};
 
 render(<Game/>, document.getElementById('app'));
