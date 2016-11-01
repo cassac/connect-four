ef560531fcefffbc2c67bc626d4ea900fe53e200
cassac <hicassac@gmail.com>
2016-11-01 10:42:26 -0700

    adds major and minor diagonal win detection

--- src/util/detect.js
+++ src/util/detect.js
@@ -47,8 +47,82 @@ const detectHorizontalVerticalWin = (board, target) => {
 
 }
 
+// major diagonal goes from top-left to bottom-right
+
+const majorDiagonalWin = (board, target) => {
+  
+  var win = false;
+
+  var recurse = (x, y, tally) => {
+    if (!tally) tally = 0;
+
+    if (board[x][y] === target) {
+      board[x][y] = undefined;
+      tally += 1;
+    }
+
+    if (board[x+1] && board[x+1][y+1] === target) {
+      board[x+1][y+1] = undefined;
+      tally += recurse(x+1, y+1, tally);
+    }
+
+    return tally;
+  }
+
+  board.forEach((row, x) => {
+    row.forEach((_, y) => {
+      if (board[x][y] === target) {
+        if(recurse(x, y) >= 4) {
+          win = true;
+        }
+      }      
+    })
+  })
+
+  return win;
+
+}
+
+// minor diagonal goes top-right to bottom-left
+
+const minorDiagonalWin = (board, target) => {
+  
+  var win = false;
+
+  var recurse = (x, y, tally) => {
+    if (!tally) tally = 0;
+
+    if (board[x][y] === target) {
+      board[x][y] = undefined;
+      tally += 1;
+    }
+
+    if (board[x+1] && board[x+1][y-1] === target) {
+      board[x+1][y-1] = undefined;
+      tally += recurse(x+1, y-1, tally);
+    }
+
+    return tally;
+  }
+
+  board.forEach((row, x) => {
+    row.forEach((_, y) => {
+      if (board[x][y] === target) {
+        if(recurse(x, y) >= 4) {
+          win = true;
+        }
+      }      
+    })
+  })
+
+  return win;
+
+}
+
 module.exports = {
   detectHorizontalVerticalWin,
+  majorDiagonalWin,
+  minorDiagonalWin,
 }
 
 // Use to test
@@ -59,5 +133,25 @@ var a = [
   [,1,,,,,],
   [,1,,,2,,],
   [,,,,,,],
+  [1,1,1,2,2,2,2]
+]
+
+var b = [
+  [2,,,,,,],
+  [,2,,,,,],
+  [,1,2,,,,],
+  [,1,1,2,2,,],
+  [,1,,1,2,,],
+  [,,,,1,,],
+  [1,1,1,2,2,2,2]
+]
+
+var c = [
+  [2,,,,,,1],
+  [,2,,,,1,1],
+  [,1,2,,1,,],
+  [,1,1,1,,,],
+  [,1,,1,2,,],
+  [,,,,1,,],
   [1,1,1,2,2,2,2]
 ]
