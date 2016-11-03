235323587cb3b40e6f3f5e3aa18239a6b87438cc
cassac <hicassac@gmail.com>
2016-11-03 15:45:21 -0700

    connects sockets, create room and emitters

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require('express')
-const socket = require('socket.io');
-const path = require('path');
 
 const app = express();
+const http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('build'));
+app.use('socket.io', express.static('node_modules/socket.io'));
+
+io.on('connection', (socket) => {
+
+  // eventually `gameroom` will be dynamic room names
+  socket.join('gameroom');
+  const numberOfPlayers = socket.adapter.rooms['gameroom'].length;
+
+  socket.emit('join', numberOfPlayers);
+
+  socket.on('turn', (data) => {
+    socket.broadcast.emit('turn', data)
+  });
+
+})
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('Connect Four running on port:', PORT);
 })
