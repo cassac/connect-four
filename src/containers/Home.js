4f7a011fd4409cd24c77582040760d2757580cf5
cassac <hicassac@gmail.com>
2016-11-04 16:55:12 -0700

    implements game room creation client side

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      room: ''
+    }
+  }
+
+  goToGameRoom() {
+    browserHistory.push('/game/'.concat(this.state.room));
+  }
+
   render() {
     return (
       <div>
-        Home Page
+        <h4>Create Game</h4>
+        <input 
+          type='text' 
+          onChange={(e) => this.setState({room: e.target.value})}
+        />
+        <input 
+          onClick={this.goToGameRoom.bind(this)}
+          type='submit' 
+          value='Create Game' 
+        />
       </div>
     )
   }
