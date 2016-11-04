f3ace7879a72960dd99cbeae13a41112a6bea55e
cassac <hicassac@gmail.com>
2016-11-04 15:44:24 -0700

    adds home container

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/App';
+import Home from './containers/Home';
 import Game from './containers/Game';
 
 render(
   <Router history={browserHistory}>
     <Route path='/' component={App} >
-      <IndexRoute component={Game} />
+      <IndexRoute component={Home} />
+      <Route path='/game/:gameRoom' component={Game} />
     </Route>
   </Router>, 
   document.getElementById('app')
