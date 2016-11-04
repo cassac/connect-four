d38d37bf32077608889459264de70fc190c5fa5c
cassac <hicassac@gmail.com>
2016-11-04 16:32:01 -0700

    adds catch all

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/App';
+import NotFound from './components/NotFound';
 import Home from './containers/Home';
 import Game from './containers/Game';
 
@@ -10,6 +11,7 @@ render(
     <Route path='/' component={App} >
       <IndexRoute component={Home} />
       <Route path='/game/:gameRoom' component={Game} />
+      <Route path='*' component={NotFound} />
     </Route>
   </Router>, 
   document.getElementById('app')
