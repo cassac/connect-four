fb4277b3b45c92306a42bcc64dc4f046225f25a0
cassac <hicassac@gmail.com>
2016-11-04 17:13:56 -0700

    adds basic layout

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
+import Divider from 'material-ui/Divider';
 
 export default class Home extends Component {
   constructor(props) {
@@ -16,16 +17,27 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <h4>Create Game</h4>
-        <input 
-          type='text' 
-          onChange={(e) => this.setState({room: e.target.value})}
-        />
-        <input 
-          onClick={this.goToGameRoom.bind(this)}
-          type='submit' 
-          value='Create Game' 
-        />
+        <div>
+          Intro Here  
+        </div>
+        <div>
+          <div>
+            <div>
+              Join a game
+            </div>
+            <Divider />
+            <h4>Create Game</h4>
+            <input 
+              type='text' 
+              onChange={(e) => this.setState({room: e.target.value})}
+            />
+            <input 
+              onClick={this.goToGameRoom.bind(this)}
+              type='submit' 
+              value='Create Game' 
+            />
+          </div>
+        </div>
       </div>
     )
   }
