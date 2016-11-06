783aa9f7bff792f38ba2dac5961fe65de6dfb33b
cassac <hicassac@gmail.com>
2016-11-06 09:32:45 -0800

    adds link to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 import AppBar from 'material-ui/AppBar';
 
 const Header = () => {
   return (
     <AppBar 
+      onTitleTouchTap={() => { browserHistory.push('/') } }
       title='Connect Four'
     />
   )
