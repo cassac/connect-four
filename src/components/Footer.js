e9ec82da0c075ab013db1804d212dabd983e2d96
cassac <hicassac@gmail.com>
2016-11-06 10:11:18 -0800

    update type

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,45 +2,34 @@ import React, {Component} from 'react';
 import FontIcon from 'material-ui/FontIcon';
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
 import Paper from 'material-ui/Paper';
-import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 
-const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>;
-const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>;
-const nearbyIcon = <IconLocationOn />;
 
-export default class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedIndex: 0,
-    };
-  }
+const Footer = ({turn, player}) => {
 
-  select(index) {
-    this.setState({selectedIndex: index});
-  } 
+  const userPlayer = <FontIcon className="material-icons">{ player }</FontIcon>;
+  const redsTurn = <FontIcon className="material-icons">RED'S</FontIcon>;
+  const bluesTurn = <FontIcon className="material-icons">BLUE'S</FontIcon>;
+  const whoseTurn = () => turn === 'a' ? 1 : 2;
 
-  render() {
-    return (
-      <Paper zDepth={1} style={{'position': 'absolute', 'bottom': '0px', 'width': '100%'}}>
-        <BottomNavigation selectedIndex={this.state.selectedIndex}>
-          <BottomNavigationItem
-            label="Turn Red"
-            icon={recentsIcon}
-            onTouchTap={() => this.select(0)}
-          />
-          <BottomNavigationItem
-            label="Turn Blue"
-            icon={favoritesIcon}
-            onTouchTap={() => this.select(1)}
-          />
-          <BottomNavigationItem
-            label="Other"
-            icon={nearbyIcon}
-            onTouchTap={() => this.select(2)}
-          />
-        </BottomNavigation>
-      </Paper>
-    );
-  }
+  return (
+    <Paper zDepth={1} style={{'position': 'absolute', 'bottom': '0px', 'width': '100%'}}>
+      <BottomNavigation selectedIndex={whoseTurn()}>
+        <BottomNavigationItem
+          icon={userPlayer}
+          label={'YOUR TEAM'}
+        />
+        <BottomNavigationItem
+          icon={redsTurn}
+          label={'TURN'}
+        />
+        <BottomNavigationItem
+          icon={bluesTurn}
+          label={'TURN'}
+        />
+
+      </BottomNavigation>
+    </Paper>
+  );
 }
+
+export default Footer;
