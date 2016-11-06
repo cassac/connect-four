676ff58aa1e0bd60c455c15a8c691d75cb0331f2
cassac <hicassac@gmail.com>
2016-11-06 11:08:19 -0800

    displays whose turn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,12 @@ import Paper from 'material-ui/Paper';
 
 const Footer = ({turn, player}) => {
 
-  const userPlayer = <FontIcon className="material-icons">{ player }</FontIcon>;
+  const redOrBlue = () => {
+    if (!player) return '';
+    else return player === 'a' ? 'RED' : 'BLUE';
+  }
+
+  const userPlayer = <FontIcon className="material-icons">{ redOrBlue() }</FontIcon>;
   const redsTurn = <FontIcon className="material-icons">RED'S</FontIcon>;
   const bluesTurn = <FontIcon className="material-icons">BLUE'S</FontIcon>;
   const whoseTurn = () => turn === 'a' ? 1 : 2;
