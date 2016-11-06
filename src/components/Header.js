c0f691df1524afa8ccfc598664c506193dc12da2
cassac <hicassac@gmail.com>
2016-11-05 17:50:36 -0700

    adds header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const Header = () => {
+  return (
+    <AppBar 
+      title='Connect Four'
+    />
+  )
+}
+
+export default Header;
