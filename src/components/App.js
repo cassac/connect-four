e6eeb80b711aea77041b21b5d390847680d38e31
cassac <hicassac@gmail.com>
2016-11-05 17:49:15 -0700

    adds global header and event plugin fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Header from './Header';
 
 class App extends Component {
   getChildContext() {
     return { muiTheme: getMuiTheme(baseTheme) };
   }
   render() {
+    injectTapEventPlugin();
     return (
       <div>
+        <Header />
         {this.props.children}
       </div>
     );
